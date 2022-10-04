10cfbd8a50b67cf321237fde1212002e51ff688e
dhrupotter <112274439+dhrupotter@users.noreply.github.com>
2022-10-04 16:50:13 +0530

    Footer links updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,20 @@ const Footer = () => {
       <div className="footer-header">Contact Me 👻</div>
       <ul className="social-links list-non-bullet">
         <li className="list-item-inline">
-          <a className="link" href="/">
+          <a className="link" href="https://twitter.com/DhruviGandhi25">
             Twitter
           </a>
         </li>
         <li className="list-item-inline">
-          <a className="link" href="/">
+          <a className="link" href="https://github.com/dhrupotter">
             Github
           </a>
         </li>
         <li className="list-item-inline">
-          <a className="link" href="/">
+          <a
+            className="link"
+            href="https://www.linkedin.com/in/dhruvi-gandhi-609a35166/"
+          >
             LinkedIn
           </a>
         </li>
